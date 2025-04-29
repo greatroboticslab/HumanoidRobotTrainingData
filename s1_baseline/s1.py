7c646057afaee9b84fc79318c2e14e468596c3dd
Sean Byler <byler@v020.ib.bridges2.psc.edu>
2025-04-29 12:32:13 -0400

    Fixed .json output for S1, added --model argument to specify desired model.

--- s1_baseline/s1.py
+++ s1_baseline/s1.py
@@ -1,3 +1,4 @@
+import argparse
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 
@@ -6,13 +7,25 @@ import json
 from os import listdir
 from os.path import isfile, join
 
+parser = argparse.ArgumentParser(description="Parse model argument")
+parser.add_argument('--model', type=str, default="s1.1-7B", help='Name or path of the model')
+args = parser.parse_args()
+model_name = args.model
+
 irrelevantToken = "!IRRELEVANT!"
 YOUTUBE_PREFIX = "https://www.youtube.com/watch?v="
+
+def IsSubtask(line):
+    if "SUBTASK" in line:
+        return True
+    return False
 
 def ExtractTask(line):
     if irrelevantToken in line:
         return irrelevantToken
-    if "TASK" in line:
+    if "MAINTASK" in line:
+        return line
+    if "SUBTASK" in line:
         return line
     return "null"
 
@@ -26,11 +39,11 @@ filenames = []
 blacklist = ""
 
 model = LLM(
-    "s1.1-7B",
+    model_name,
     tensor_parallel_size=4,
     disable_custom_all_reduce=True
 )
-tok = AutoTokenizer.from_pretrained("s1.1-7B")
+tok = AutoTokenizer.from_pretrained(model_name)
 
 stop_token_ids = tok("<|im_end|>")["input_ids"]
 
@@ -44,6 +57,7 @@ mypath = "../video_processing/transcripts/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 jsonData = []
 allTasks = []
+allSubtasks = []
 relevantCount = 0
 irrelevantCount = 0
 
@@ -52,8 +66,8 @@ for file in onlyfiles:
     if(str(file[0]) != '.'):
         if True: #try:
             tasks = []
-            print(str(file[0]))
-            print(str(file))
+            #print(str(file[0]))
+            #print(str(file))
             fi = open(mypath + file, "r", encoding="ascii", errors="ignore")
 
             vID = os.path.splitext(file)[0]
@@ -71,11 +85,16 @@ for file in onlyfiles:
                 prompt = "<|im_start|>system\nYou are Qwen, a helful assistant. "
                 prompt += "You will be given a video transcript and asked to generate a series of tasks "
                 prompt += "based on the transcript that a person would have to perform. "
-                prompt += "Give one task per line. Write TASK: before every task. "
-                prompt += "Give tasks and only tasks, do not discuss or talk about anything else. "
+                prompt += "A task should be a generalization, and made up of smaller sub-tasks. "
+                prompt += "Give one task per line. Write MAINTASK: before every task. "
+                prompt += "After writing MAINTASK: give a list of subtasks, one per line. "
+                prompt += "A for each subtask, write SUBTASK: before every subtask. "
+                prompt += "When you are finished with the subtasks for a task, you can start "
+                prompt += "a new task by writing MAINTASK: and then the new general task. "
+                prompt += "Give only tasks and subtasks, do not discuss or talk about anything else. "
                 prompt += "Do not go into detail on how you made each task, just give the tasks. "
-                prompt += "Only include tasks that are related to farming, agriculture, or operating farming equiptment."
-                prompt += "However, if you feel that the transcript has nothing to do with the tasks of performing physical farming tasks, then simply say " + irrelevantToken + " all caps, exclamation points surrounding it. "
+                prompt += "Only include tasks and subtasks that are related to farming, agriculture, or operating farming equiptment."
+                prompt += "However, if you feel that the transcript has nothing to do with the tasks of performing physical farming, husbandry, or agricultural tasks, then simply say " + irrelevantToken + " all caps, exclamation points surrounding it. "
                 prompt += "The entire transcript must be irrelevant. If it is still somewhat relevant, just save the tasks of the relevant actions."
                 prompt += "<|im_end|>\n"
                 prompt += "<|im_start|>user\nGiven this transcript, please generate a list of physical tasks a person would have to perform with their body in relation to the transcript. Separate the tasks by a new line character:"
@@ -88,7 +107,8 @@ for file in onlyfiles:
                 o = model.generate(prompt, sampling_params=sampling_params)
                 #print(o[0].outputs[0].text)
                 lines = o[0].outputs[0].text.splitlines()
-
+                
+                curTask = -1
                 for l in lines:
                     task = ExtractTask(l)
                     if task != "null":
@@ -98,34 +118,51 @@ for file in onlyfiles:
                         else:
                             # Accept
                         
-                            print(l)
+                            # print(l)
                             motion = TaskToMoMask(task)
-                            tasks.append(motion)
+                            if IsSubtask(l):
+                                if len(tasks) > 0:
+                                    tasks[curTask].append(motion)
+                            else:
+                                newTask = [motion]
+                                tasks.append(newTask)
+                                curTask += 1
+                            
+                            # tasks.append(motion)
                             
                 
-            except:
+            except Exception as e:
                 relevant = False
-                print("File IO error, skipping...")
+                print("File Error: ", e)
                 
 
             if relevant:
-                print("Relevant video, saving tasks...")
+                print(str(file) + ": relevant video, saving tasks...")
                 relevantCount += 1
                 for t in tasks:
-                    allTasks.append(t)
+                    for s in t:
+                        allTasks.append(s)
 
                 # Output info CSV
                 
+                jTasks = []
+                for t in tasks:
+                    jTask = {
+                         "task": t[0],
+                         "subtasks": t[1:]
+                    }
+                    jTasks.append(jTask)
+
                 entry = {
                     "index": vID,
                     "title": videoTitle,
                     "url": url,
-                    "tasks": tasks
+                    "tasks": jTasks
                 }
                 jsonData.append(entry)
 
             else:
-                print("Irrelevant video, blacklisting...")
+                print(str(file) + ": irrelevant video, blacklisting...")
                 irrelevantCount += 1
                 blacklist += url + "\n"
 
