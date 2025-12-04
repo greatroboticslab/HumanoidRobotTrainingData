b79119d4970d1d151f0e51099942d643a1798ba9
anushakram <reachanushabk@gmail.com>
2025-12-04 12:20:38 -0500

    Updated repo.

--- momask_baseline/scripts/generate_all.sh
+++ momask_baseline/scripts/generate_all.sh
@@ -8,6 +8,6 @@ f=0
 while [ $f -lt $MAX ]; do
     f=$((s + 100))
     echo "Submitting Jobs: $s - $f"
-    sbatch generate_motions.slurm "s" "f"
+    sbatch generate_motions.slurm "$s" "$f"
     s=$((s + 100))
 done
